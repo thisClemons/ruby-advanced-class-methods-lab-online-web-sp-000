756a6df89b842c165811881e91868518004b5371
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-25 18:24:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,7 +51,7 @@ class Song
 
   def self.new_from_filename(filename)
     parsed = filename.split(/ - |.mp3/)
-    song = Song.new_by_name(parsed[1])
+    song = self.new_by_name(parsed[1])
     song.artist_name = parsed[0]
     binding.pry
 
