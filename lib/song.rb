d28e926c10efcc9b02c0a27b33ec5ea568dac491
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-25 17:55:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -36,6 +38,7 @@ class Song
   end
 
   def find_or_create_by_name(name)
+    binding.pry
     if self.find_by_name(name)
       self.find_by_name(name)
     else self.create_by_name(name)
